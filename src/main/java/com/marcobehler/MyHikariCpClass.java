d4137afc1ec8b07d336056bd9ef87f8207d31e63
marco <marco@marcobehler.com>
2020-04-30 13:58:37 +0200

    MyHikariCP class

---
+++ src/main/java/com/marcobehler/MyHikariCpClass.java
@@ -0,0 +1,33 @@
+package com.marcobehler;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class MyHikariCpClass {
+
+    public static void main(String[] args) {
+        DataSource dataSource = createDataSource();
+
+        try (Connection conn = dataSource.getConnection()) {
+
+            // "0" means disabling the timeout, when doing isValid checks
+            boolean isValid = conn.isValid(0);
+            System.out.println("Do we have a valid db connection? = " + isValid);
+
+            // Do something with the Connection, run some SQL statements
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static DataSource createDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:mysql://localhost/test?serverTimezone=UTC");
+        ds.setUsername("myUsername");
+        ds.setPassword("myPassword");
+        return ds;
+    }
+}
