eba1368178868dc97ff1eb1824f07599f44454ec
marco <marco@marcobehler.com>
2020-04-30 15:51:27 +0200

    JdbcQUeries class

---
+++ src/main/java/com/marcobehler/JdbcQueries.java
@@ -0,0 +1,32 @@
+package com.marcobehler;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcQueries {
+
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager
+                .getConnection("jdbc:mysql://localhost/test?serverTimezone=UTC",
+                        "myUsername", "myPassword")) {
+
+            List<User> users = new ArrayList<>();
+
+            PreparedStatement selectStatement = conn.prepareStatement("select * from users");
+            ResultSet rs = selectStatement.executeQuery();
+
+            while (rs.next()) { // will traverse through all rows
+                Integer id = rs.getInt("id");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+
+                User user = new User(id, firstName, lastName);
+                users.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
